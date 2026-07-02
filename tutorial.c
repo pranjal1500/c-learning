e7222fd708e78f8f1a08d10e4a51f1c594f9f45c
Pranjal1500 <swatiuphade8@gmail.com>
2026-07-02 22:42:17 +0530

    changed the program to Hot and cold

--- tutorial.c
+++ tutorial.c
@@ -1,35 +1,20 @@
-/*Write a function to find square root of a number*/
+/*create the function to print hot and cold depending on the temperature the user enters*/
 #include <stdio.h>
-int sqrtNumber(int n);
+
+void Temp(int temp);
 int main() {
-    int n;
-    int sqrt;
-    printf("Enter number to find square root : ");
-    scanf("%d",&n);
-    sqrt = sqrtNumber(n);
-    if(sqrt == -1){
-        printf("The square root of %d is not a integer",n);
-    } else {
-        printf("The square root of %d is %d",n, sqrt);
-    }
+    int temp;
+    printf("enter temprature : ");
+    scanf("%d",&temp);
+    Temp(temp);
     return 0;
 }
 
-int sqrtNumber(int n){
-    int root;
-    int found = 0;
-    for(int i = 0; i*i<=n; i++){
-        if(i*i==n){
-            root = i;
-            found = 1;
-            break;
-        }
+void Temp(int temp){
+    if(temp<=30){
+        printf("Cold");
+    } else if(temp>30){
+        printf("Hot");
     }
-
-    if(found){
-       return root;
-    } else {
-        return -1;
-    }
-
 }
+
