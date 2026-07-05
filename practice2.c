7867ab60d8105bf5dd0be4555c17c2e8d7c5913b
Pranjal1500 <swatiuphade8@gmail.com>
2026-07-05 10:59:58 +0530

    Revised recursion concept

---
+++ practice2.c
@@ -0,0 +1,17 @@
+/*calculate factorial of number using recursion*/
+#include <stdio.h>
+int fact(int n);
+int main(){
+    int n;
+    printf("Enter number : ");
+    scanf("%d",&n);
+    printf("Factorial of %d is : %d",n,fact(n));
+    return 0;
+}
+
+int fact(int n){
+    if(n==0){
+        return 1;
+    }
+    return n * fact(n-1);
+}
