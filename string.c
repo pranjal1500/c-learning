973d206167a5bc9513590d1188e0a3c44906cb20
Pranjal1500 <swatiuphade8@gmail.com>
2026-07-07 09:53:30 +0530

    learning strings

---
+++ string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void printString(char arr[]);
+int main(){
+    char firstName[]="Pranjal";
+    char lastName[] = "Uphade";
+
+    printString(firstName);
+    // printf("\n");
+    printString(lastName);
+    return 0;
+}
+
+void printString(char arr[]){
+    for(int i=0; arr[i]!='\0';i++){
+        printf("%c",arr[i]);
+    }
+    printf("\n");
+}
